5eda69d2dda231bd41e4eafe8bbdf3084898f4ac
Andrés Mauricio Martínez Hincapié - Ceiba Software <andres.hincapie@ceiba.com.co>
2022-04-04 23:41:05 -0500

    Ajuste prueba Layout

--- src/app/feature/comparendo/components/layout/layout.component.spec.ts
+++ src/app/feature/comparendo/components/layout/layout.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
 
-describe('LayoutComponent', () => {
+describe('Comparendo > LayoutComponent', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
 
